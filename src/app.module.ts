3346ce4d97cb5b4d6f02f5d3fde4fc9e47d11e36
Arsen <aarakelyan98@gmail.com>
2022-11-19 20:47:27 +0100

    feat: added user and course entities templates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
+import { UserModule } from './user/user.module';
+import { CourseModule } from './course/course.module';
 
 @Module({
 	controllers: [AppController],
 	providers: [AppService],
+	imports: [UserModule, CourseModule],
 })
 export class AppModule {}
